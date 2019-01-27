736a8d84067d7fc1a35fb3f9257c1aabf996e14a
zhangpenghao-pd <zhangpenghao-pd@360.cn>
2019-01-27 13:19:22 +0800

    for plugins.go

---
+++ clientool/init.go
@@ -0,0 +1,5 @@
+package clientool
+
+import (
+	_ "github.com/Qihoo360/wayne/src/backend/plugins/clientool/routers"
+)
--- plugins.go
+++ plugins.go
@@ -1,5 +1,6 @@
 package plugins
 
 import (
+	_ "github.com/Qihoo360/wayne/src/backend/plugins/clientool"
 	_ "github.com/Qihoo360/wayne/src/backend/plugins/service"
 )
